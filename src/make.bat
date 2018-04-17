3fd435c57299aca066868e2b2124f5b796eb8f32
rorywalsh <rorywalsh@ear.ie>
2018-04-17 20:30:59 +0100

    updating windows make file

--- src/make.bat
+++ src/make.bat
@@ -3,7 +3,7 @@ echo ""
 echo "PLEASE EDIT THE SCRIPTS VARIABLES"
 echo "SO THAT IT USES THE CORRECT PATH"
 
-set PROCESSING_LIB=C:\Program Files\processing-2.2.1\core\library\core.jar
-set CSOUND_LIB=C:\Users\rory\Documents\sourcecode\cabbageaudio\csound\build\csnd6.jar
-javac -classpath %PROCESSING_LIB%:%CSOUND_LIB% -d . *.java
+set PROCESSING_LIB=C:\Users\rory\sourcecode\processing-3.3.7\core\library\core.jar
+set CSOUND_LIB=C:\PROGRA~1\Csound6_x64\bin\csnd6.jar
+javac -classpath %PROCESSING_LIB%;%CSOUND_LIB% -d . *.java
 jar -cf ../library/csoundo.jar ./csoundo/*.class
